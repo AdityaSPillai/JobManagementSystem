9302cc84208698ba73f1b71d82807128c4baa008
Aditya S <119068813+AdityaSPillai@users.noreply.github.com>
2025-11-05 18:21:46 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -33,7 +33,7 @@ app.use(cors({
     'http://localhost:5174',
     'http://127.0.0.1:5173',
     'http://127.0.0.1:5174',
-    'https://your-frontend.com'
+    'https://jobmanagementsystem-1.onrender.com/'
   ],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
@@ -61,4 +61,5 @@ const PORT= process.env.PORT || 5000
 
 app.listen(PORT,()=>{
     console.log(`App is listening on port ${PORT}`);
+
 })
