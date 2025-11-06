1a4d6dbef9d1ae17cfac28f8f231556c326dda11
Aditya S <119068813+AdityaSPillai@users.noreply.github.com>
2025-11-06 09:28:12 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -58,7 +58,7 @@ app.use('/v1/jobs',jobRoutes)
 app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
 // ✅ Catch-all route for React Router
-app.get("/.*/", (req, res) => {
+app.use((req, res, next) => {
   res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
 });
 
